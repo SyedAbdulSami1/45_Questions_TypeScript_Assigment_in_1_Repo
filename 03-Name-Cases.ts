76b8e42c813569168bbbe51563be5696874b76bc
SyedAbdulSami1 <samiwpp@gmail.com>
2024-03-25 08:13:33 +0500

    work of 5

--- 03-Name-Cases.ts
+++ 03-Name-Cases.ts
@@ -1,10 +1,10 @@
 // 03-Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
 
-let personName3: string = "Sami";
+let p3Name: string = "Sami";
 
-console.log("Name in Upper Case:", personName3.toUpperCase());
-console.log("Name in Lower Case:", personName3.toLowerCase());
+console.log("Name in Upper Case:", p3Name.toUpperCase());
+console.log("Name in Lower Case:", p3Name.toLowerCase());
 
-let firstName = personName3.charAt(0).toUpperCase();
-let lastName = personName3.slice(1).toLowerCase();
+let firstName = p3Name.charAt(0).toUpperCase();
+let lastName = p3Name.slice(1).toLowerCase();
 console.log("Name in Title Case:", firstName + lastName);
