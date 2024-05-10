8da10da22abbbb6c6de02b1d83c34ecd615514e6
SyedAbdulSami1 <samiwpp@gmail.com>
2024-05-11 01:33:54 +0500

    change working pattern

--- dist/02-Personal-Message.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// 02-Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
-let p2Name = "Eric";
-console.log(`"Hello ${p2Name}, would you like to learn some Python today?`);
--- dist/03-Name-Cases.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// 03-Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
-let p3Name = "Sami";
-console.log("Name in Upper Case:", p3Name.toUpperCase());
-console.log("Name in Lower Case:", p3Name.toLowerCase());
-let firstName = p3Name.charAt(0).toUpperCase();
-let lastName = p3Name.slice(1).toLowerCase();
-console.log("Name in Title Case:", firstName + lastName);
