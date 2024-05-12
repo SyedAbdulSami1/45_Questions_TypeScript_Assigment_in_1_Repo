9c8c5196fd6874ee6e04ed115517035c62ac32e9
SyedAbdulSami1 <samiwpp@gmail.com>
2024-05-12 22:30:15 +0500

    08  is final

---
+++ 08-creat-four-lines/08-creat-four-lines.js
@@ -0,0 +1,13 @@
+/*8. You should create four lines that look like this:
+_____________________________________________
+console.log(5 + 3)
+____________________________________________
+Your output should simply be four lines with the number 8 appearing once on each line.*/
+console.log(5 + 3);
+console.log("-".repeat(50));
+console.log(10 - 2);
+console.log("-".repeat(50));
+console.log(2 * 4);
+console.log("-".repeat(50));
+console.log(16 / 2);
+console.log("-".repeat(50));
