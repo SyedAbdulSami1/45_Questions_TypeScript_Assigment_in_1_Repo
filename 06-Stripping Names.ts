8b05716b1763bcc264bb89e357ecba9471c04b51
SyedAbdulSami1 <samiwpp@gmail.com>
2024-05-12 17:27:54 +0500

    work on 6

--- 06-Stripping Names.ts
+++ 06-Stripping Names.ts
@@ -3,3 +3,6 @@ and include some whitespace characters at the beginning and end of the name.
 Make sure you use each character combination, "\t" and "\n", at least once. 
 Print the name once, so the whitespace around the name is displayed. 
 Then print the name after striping the white spaces.*/
+
+const personName:string="\t \n Syed Abdul Sami \n \t";
+console.log(personName);
