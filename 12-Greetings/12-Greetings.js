6e8ecfed00ea16bf06b30aed80728fe45425fe2f
SyedAbdulSami1 <samiwpp@gmail.com>
2024-05-28 17:16:53 +0500

    question 12

---
+++ 12-Greetings/12-Greetings.js
@@ -0,0 +1,10 @@
+/*12. Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message
+should be the same, but each message should be personalized with the person’s name.*/
+var friendsArray = ["Waqar", "Hassam", "Asif", "Naveed", "Ali", "Salman"];
+var i = 0;
+for (var _i = 0, friendsArray_1 = friendsArray; _i < friendsArray_1.length; _i++) {
+    var myFriend = friendsArray_1[_i];
+    console.log("Congratulations ".concat(friendsArray[i], " for clearing your first exam."));
+    i++;
+}
+;
---
+++ 12-Greetings/12-Greetings.ts
@@ -0,0 +1,9 @@
+/*12. Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message
+should be the same, but each message should be personalized with the person’s name.*/
+
+const friendsArray:string[]=["Waqar", "Hassam", "Asif", "Naveed", "Ali", "Salman"];
+let i:number=0;
+for (const myFriend of friendsArray) {
+    console.log(`Congratulations ${friendsArray[i]} for clearing your first exam.`);
+    i++
+};
