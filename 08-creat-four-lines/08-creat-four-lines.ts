60a461d2a6f6554ca28eca5c9cb6bf891e4e305b
SyedAbdulSami1 <samiwpp@gmail.com>
2024-05-28 16:08:00 +0500

    removed Repeate method

--- 08-creat-four-lines/08-creat-four-lines.ts
+++ 08-creat-four-lines/08-creat-four-lines.ts
@@ -5,10 +5,10 @@ ____________________________________________
 Your output should simply be four lines with the number 8 appearing once on each line.*/
 
 console.log(5+3);
-console.log("-".repeat(50));
+console.log("-----------------*-----------------");
 console.log(10-2);
-console.log("-".repeat(50));
+console.log("-----------------*-----------------");
 console.log(2*4);
-console.log("-".repeat(50));
+console.log("-----------------*-----------------");
 console.log(16/2);
-console.log("-".repeat(50));
+console.log("-----------------*-----------------");
