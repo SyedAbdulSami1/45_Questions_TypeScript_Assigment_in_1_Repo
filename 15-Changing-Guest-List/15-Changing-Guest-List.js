56fa0169ebbc4095f6c225fbfdfb6036b81f0019
SyedAbdulSami1 <samiwpp@gmail.com>
2024-05-28 19:53:39 +0500

    question 15

---
+++ 15-Changing-Guest-List/15-Changing-Guest-List.js
@@ -0,0 +1,22 @@
+/*15-Changing-Guest-List: You just heard that one of your guests can’t make the dinner, so you
+need to send out a new set of invitations. You’ll have to think of someone else to invite.
+
+• Start with your program from Exercise 14. Add a print statement at the end of
+your program stating the name of the guest who can’t make it.
+
+• Modify your list, replacing the name of the guest who can’t make it with
+the name of the new person you are inviting.
+
+• Print a second set of invitation messages, one for each person who is still in your list*/
+var guestList = ["Waqar", "Hassam", "Asif"];
+console.log("".concat(guestList[2], " excused himself from coming for dinner."));
+guestList.splice(2, 1, "Naveed");
+console.log(guestList);
+for (var i = 0; i <= guestList.length; i++) {
+    if (i == 0) {
+        console.log("My new dinner list with these new persons");
+    }
+    else {
+        console.log(guestList[i - 1]);
+    }
+}
