543b55d2316cbf76ed3fe13c511f24346cf554b2
Syed Abdul Sami <samiwpp@gmail.com>
2024-06-09 04:46:42 +0500

    19

---
+++ 19-Dinner-Guests/19-Dinner-Guests.ts
@@ -0,0 +1,4 @@
+/*19-Dinner-Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.*/
+const guestList : string[] = ["Waqar", "Hassam", "Asif"];
+console.log(`I am invited to dinner only ${guestList.length} person.`);
+
