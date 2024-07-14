dc98b3fe64793b75b4875d673ea68b529de55d22
Syed Abdul Sami <samiwpp@gmail.com>
2024-07-14 12:54:17 +0500

    23-Conditional-Tests

---
+++ 23-Conditional-Tests/23-Conditional-Tests.ts
@@ -0,0 +1,36 @@
+/*23. Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.")
+console.log(car == 'subaru')
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.*/
+//1 Test with ture and false
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.")
+console.log(car == 'subaru')
+console.log("Is car == 'Honda'? I predict False.")
+console.log(car == 'Honda')
+//2 Test with ture and false
+let apple = 'fruit';
+console.log("Is apple == 'fruit'? I predict True.");
+console.log(apple == 'fruit');
+console.log("Is apple == 'vagitable'? I predict False.");
+console.log(apple == 'vagitable');
+//3 Test with ture and false
+let dog = "animal";
+console.log("Is dog == 'animal'? I predict True.");
+console.log(dog == 'animal');
+console.log("Is dog == 'bird'? I predict False.");
+console.log(dog == 'bird');
+//4 Test with ture and false
+let coldDrink = "Liquid";
+console.log("Is coldDrink == 'Liquid'? I predict True.");
+console.log(coldDrink == 'Liquid');
+console.log("Is coldDrink == 'Solid'? I predict False.");
+console.log(coldDrink == 'Solid');
+//5 Test with ture and false
+let cricket = "Sports";
+console.log("Is cricket == 'Sports'? I predict True.");
+console.log(cricket == 'Sports');
+console.log("Is cricket == 'Movie'? I predict False.");
+ console.log(cricket == 'Movie');
